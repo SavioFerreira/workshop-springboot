a093686b68fb7f4ae4936493d09e00f9530e70cf
Savio Ferreira <ferreirasavio39@gmail.com>
2023-01-28 23:44:06 -0300

    Project created

---
+++ src/test/java/com/savi/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savi.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
